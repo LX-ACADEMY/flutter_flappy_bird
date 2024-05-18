e7f4f83d5707ab1b63a0bfc1a5482532c8613791
MohammedAzharKK <azramn6993@gmail.com>
2024-05-18 16:38:39 +0530

    bird positioned

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -2,9 +2,20 @@ import 'package:flappy_bird/view/widgets/bird.dart';
 import 'package:flappy_bird/view/widgets/piller_part.dart';
 import 'package:flappy_bird/view/widgets/score_board.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +25,15 @@ class HomePage extends StatelessWidget {
               image: AssetImage('assets/bj.png'), fit: BoxFit.fill)),
       height: double.infinity,
       width: double.infinity,
-      child: const Stack(
+      child: Stack(
         children: [
-          PillerPart(),
-          ScoreBoard(),
-          Center(child: Bird()),
+          const PillerPart(),
+          const ScoreBoard(),
+          Positioned(
+            top: MediaQuery.sizeOf(context).height * 0.5 - 40,
+            left: MediaQuery.sizeOf(context).width * 0.5 - 40,
+            child: const Bird(),
+          ),
         ],
       ),
     );
--- lib/view/widgets/bird.dart
+++ lib/view/widgets/bird.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Bird extends StatelessWidget {
+class Bird extends StatefulWidget {
   const Bird({super.key});
 
+  @override
+  State<Bird> createState() => _BirdState();
+}
+
+class _BirdState extends State<Bird> {
   @override
   Widget build(BuildContext context) {
     return Container(
