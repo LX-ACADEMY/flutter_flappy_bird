468b4f96925875cb63ab86e6fb90105841a8f219
Karthikrajendranofficial <karthikrajendranofficial@gmail.com>
2024-05-20 10:29:17 +0530

    piller first

--- lib/view/widgets/piller_part.dart
+++ lib/view/widgets/piller_part.dart
@@ -5,6 +5,56 @@ class PillerPart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        if (index % 2 == 0) {
+          return Container(
+            height: double.infinity,
+            color: Colors.transparent,
+            width: 100,
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: const Color.fromARGB(255, 234, 111, 4),
+                ),
+                const Spacer(),
+                Container(
+                  width: 100,
+                  height: 200,
+                  color: const Color.fromARGB(255, 234, 111, 4),
+                ),
+                Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/Pasted image.png"),
+                            fit: BoxFit.fill))),
+              ],
+            ),
+          );
+        } else {
+          return Container(
+            height: double.infinity,
+            width: 100,
+            child: Column(
+              children: [
+                const Spacer(),
+                Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/Pasted image.png"),
+                            fit: BoxFit.fill))),
+              ],
+            ),
+          );
+        }
+      },
+    );
   }
 }
