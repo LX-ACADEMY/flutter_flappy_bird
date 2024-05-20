64ccbfa1d60d3feb3a05915445b25f6f298ed11c
Karthikrajendranofficial <karthikrajendranofficial@gmail.com>
2024-05-20 15:22:01 +0530

    piller full

--- lib/view/widgets/piller_part.dart
+++ lib/view/widgets/piller_part.dart
@@ -3,12 +3,14 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class PillerPart extends StatelessWidget {
-  // var randomObject = Random();
-
   PillerPart({super.key});
 
+  late double intValue;
+
+  // var randomObject = Random();
   @override
   Widget build(BuildContext context) {
+    double pillerFullWidth = MediaQuery.of(context).size.height - 300;
     return ListView.builder(
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
@@ -21,20 +23,29 @@ class PillerPart extends StatelessWidget {
               children: [
                 Container(
                   width: double.infinity,
-                  height: 200,
-                  child: Image.asset(
-                    "assets/Pasted image (2).png",
-                    fit: BoxFit.fill,
+                  height: intValue =
+                      Random().nextInt(pillerFullWidth.toInt()).toDouble(),
+                  // child: Image.asset(
+                  //   "assets/Pasted image (2).png",
+                  //   fit: BoxFit.fill,
+                  // ),
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/Pasted image (2).png"),
+                      fit: BoxFit.fitWidth,
+                      alignment: FractionalOffset.bottomCenter,
+                    ),
                   ),
                 ),
                 const Spacer(),
                 Container(
                   width: 100,
-                  height: 200,
-                  child: Image.asset(
-                    "assets/Pasted image (3).png",
-                    fit: BoxFit.fill,
-                  ),
+                  height: pillerFullWidth - intValue,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/Pasted image (3).png"),
+                          fit: BoxFit.fitWidth,
+                          alignment: FractionalOffset.topCenter)),
                 ),
                 Container(
                     width: double.infinity,
@@ -49,7 +60,7 @@ class PillerPart extends StatelessWidget {
         } else {
           return Container(
             height: double.infinity,
-            width: 150,
+            width: 200,
             child: Column(
               children: [
                 const Spacer(),
